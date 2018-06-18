55ccbbf53f10766c7e176dce9c260f1d89be8097
Scott Stolzman <scotts@azasrs.gov>
2018-06-18 13:51:53 -0600

    Add navs functionality

--- scripts/calcs_privm.R
+++ scripts/calcs_privm.R
@@ -1,5 +1,7 @@
 library('tidyverse')
 
+#TODO: all_navs function - no great way to capture date for 'perf' (fundinfo.csv workaround needs fixing)
+
 
 all_cashflow_irr = function(){
   x.all=read_csv(file='data/AllCashFlowIRR.csv')
@@ -27,4 +29,54 @@ all_cashflow_irr = function(){
     mutate(Date = as.Date(Date, format='%m/%d/%Y'))
   return(df)
 }
+
+
+all_navs = function(){
+  perf = read_csv(file = 'data/Valuation Performance.csv')
+  hv = read_csv(file='data/AllHistoricalMV.csv')
+  ## fundinfo.csv only being brought in for a date to be pulled (which is updated manually each qtr??)
+  fundinfo = read_csv(file='data/fundinfo.csv')
+  
+  df_perf = perf %>%
+    select(-`Investment Name`) %>%
+    rename(ShortName = `Client Grouping 1`,
+           Amount = `Market Value`) %>%
+    mutate(Type = "V",
+           Date = as.Date(zoo::as.yearqtr(Sys.Date()) - 1/4, frac = 1))
+  ### this is where the "quarter" date is made up from manual data update
+  ## Could we use??
+  # as.Date(zoo::as.yearqtr(Sys.Date()) + 1/4) = beginning of quarter
+  # as.Date(zoo::as.yearqtr(Sys.Date()) + 1/4, frac = 1) = end of quarter
+  # as.Date(zoo::as.yearqtr(Sys.Date()) - 1/4, frac = 1) = end of quarter ??
+  
+  df_hv = hv %>%
+    mutate(Date = as.Date(Date, format = '%m/%d/%Y'))
+  
+  ### check required for NA values
+  df = bind_rows(df_hv, df_perf)
+  return(df)
+}
+
+
+
+x.all = all_cashflow_irr()
+hv = all_navs()
+a = x.all %>% left_join(fundinfo, by = c('ShortName'='Short'))
+b = hv %>% left_join(fundinfo, by = c('ShortName' = 'Short'))
+
+#106 - 
+#join fundinfo and cats, some random fiddling done to make it fit
+
+#124 - 154
+#join x and f.c.name, join x.v, join hv (where x.v is subset of x.all)
+
+# 156 - 
+# join x and cats
+
+
+tfundvals = read_csv('data/tfundvals.csv')
+p2pirrs = read_csv('data/p2pirrs.csv') #seems created not raw data
+portmat = read_csv('data/portmat.csv') #seems created not raw data
+
+
 
